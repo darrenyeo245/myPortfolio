b440ab1f150a15284139224cc41155b4cacfc719
YangReiskanzler <darrenyeo245@gmail.com>
2025-08-19 12:15:49 +0200

    chore:save changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,21 @@
-module.exports = {
-    darkMode: 'selector', '[data-mode="dark"]'
+export default {
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                text: "var(--color-text)",
+                hover: "var(--color-hover)",
+                border: "var(--color-border)",
+                background: "var(--color-background)",
+                secondarybackground: "var(--color-secondarybackground)",
+                accent: "var(--color-accent)",
+                textDark: "var(--color-text-dark)",
+                hoverDark: "var(--color-hover-dark)",
+                borderDark: "var(--color-border-dark)",
+                backgroundDark: "var(--color-background-dark)",
+                secondarybackgroundDark: "var(--color-secondarybackground-dark)",
+                accentDark: "var(--color-accent-dark)",
+            },
+        },
+    },
 };
