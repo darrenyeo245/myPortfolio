6b9ab781fa20c61875897d1b141a782521b17f63
YangReiskanzler <darrenyeo245@gmail.com>
2025-08-23 18:30:09 +0200

    feat: add url for skills

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -3,28 +3,35 @@ import React from "react";
 export interface Skill {
     name: string;
     icon: string;
+    url?: string;
     }
 
 export const skills: Skill[] = [
 {
     name: "HTML",
-    icon: "devicon-html5-plain colored"
+    icon: "devicon-html5-plain colored",
+    url: "https://developer.mozilla.org/en-US/docs/Web/HTML"
     },
 {
     name: "Python",
-    icon: "devicon-python-plain colored"
+    icon: "devicon-python-plain colored",
+    url: "https://www.python.org/"
     }
 ,
 {    name: "TypeScript",
-    icon: "devicon-typescript-plain colored"
+    icon: "devicon-typescript-plain colored",
+    url: "https://www.typescriptlang.org/"
     },
 {    name: "React",
-    icon: "devicon-react-plain colored"
+    icon: "devicon-react-plain colored",
+    url: "https://react.dev/"
     },
 {    name: "Astro",
-    icon: "devicon-astro-plain colored"
+    icon: "devicon-astro-plain colored",
+    url: "https://astro.build/"
     },
 {    name: "Tailwind CSS",
-    icon: "devicon-tailwindcss-plain colored"
+    icon: "devicon-tailwindcss-plain colored",
+    url: "https://tailwindcss.com/"
     }
 ];
