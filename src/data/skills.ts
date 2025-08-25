90eba1dc56b28ac8ba6446f601793eb3b1c9d9b5
YangReiskanzler <darrenyeo245@gmail.com>
2025-08-25 20:47:14 +0200

    chore: reformat code

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -4,34 +4,38 @@ export interface Skill {
     name: string;
     icon: string;
     url?: string;
-    }
+}
 
 export const skills: Skill[] = [
-{
-    name: "HTML",
-    icon: "devicon-html5-plain colored",
-    url: "https://developer.mozilla.org/en-US/docs/Web/HTML"
+    {
+        name: "HTML",
+        icon: "devicon-html5-plain colored",
+        url: "https://developer.mozilla.org/en-US/docs/Web/HTML"
     },
-{
-    name: "Python",
-    icon: "devicon-python-plain colored",
-    url: "https://www.python.org/"
+    {
+        name: "Python",
+        icon: "devicon-python-plain colored",
+        url: "https://www.python.org/"
     }
-,
-{    name: "TypeScript",
-    icon: "devicon-typescript-plain colored",
-    url: "https://www.typescriptlang.org/"
+    ,
+    {
+        name: "TypeScript",
+        icon: "devicon-typescript-plain colored",
+        url: "https://www.typescriptlang.org/"
     },
-{    name: "React",
-    icon: "devicon-react-plain colored",
-    url: "https://react.dev/"
+    {
+        name: "React",
+        icon: "devicon-react-plain colored",
+        url: "https://react.dev/"
     },
-{    name: "Astro",
-    icon: "devicon-astro-plain colored",
-    url: "https://astro.build/"
+    {
+        name: "Astro",
+        icon: "devicon-astro-plain colored",
+        url: "https://astro.build/"
     },
-{    name: "Tailwind CSS",
-    icon: "devicon-tailwindcss-plain colored",
-    url: "https://tailwindcss.com/"
+    {
+        name: "Tailwind CSS",
+        icon: "devicon-tailwindcss-plain colored",
+        url: "https://tailwindcss.com/"
     }
 ];
